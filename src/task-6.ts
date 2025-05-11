ad2cfd4bb17ddce2877d23db96f09e8ca195922e
Yuliia Hubska <zhukapuka10@gmail.com>
2025-05-11 21:18:29 +0300

    fix task 6

--- src/task-6.ts
+++ src/task-6.ts
@@ -8,6 +8,6 @@
 function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
-getFirstElement([1, 2, 3]); // 1
-getFirstElement(["a", "b", "c"]); // "a"
-getFirstElement([true, false, true]); // true
+getFirstElement<number>([1, 2, 3]); // 1
+getFirstElement<string>(["a", "b", "c"]); // "a"
+getFirstElement<boolean>([true, false, true]); // true
